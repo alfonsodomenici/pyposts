0bd4a842587459fae6d1b5b7e43880d6431bde04
Alfonso Domenici <alfonso.domenici@gmail.com>
2023-10-19 17:11:35 +0200

    sqlalchemy wuth sqlite

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config.from_object(Config)
 
-from app import routes
+"""
+L'oggetto db rappresenta il database e fornisce una api con tutte le funzionalità necsesarie
+"""
+db = SQLAlchemy(app)
+
+from app import routes, models
