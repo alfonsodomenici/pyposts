d21126b3c16b80122ed2139c93da443509f96279
Alfonso Domenici <alfonso.domenici@gmail.com>
2023-10-26 09:55:09 +0200

    resource_owner_required decorator

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -1,7 +1,10 @@
+import json
+from flask import current_app
 from functools import wraps
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
 from app.api.responses import response_with
 from app.api import responses as resp
+from app.exceptions import NotResourceOwnerError
 
 def admin_required():
     def wrapper(fn):
@@ -14,5 +17,26 @@ def admin_required():
                 return fn(*args,**kwargs)
             else:
                 return response_with(resp.FORBIDDEN_403_ONLY_ADMIN)
+        return decorator
+    return wrapper
+
+def resource_owner_required(payload_name: str ='user',payload_field: int ='id'):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            try:
+                verify_jwt_in_request()
+                claims=get_jwt()
+                role=claims['role']
+                sub_id=claims['sub_id']
+                result = fn(*args,**kwargs)
+                payload=json.loads(result.response[0])
+                current_app.logger.info(result.response)
+                if payload[payload_name][payload_field] != sub_id and role != 'ADMIN':
+                    raise NotResourceOwnerError('not owner..')
+                return result
+            except Exception as e:
+                current_app.logger.error(e,exc_info=True)
+                raise NotResourceOwnerError('error i decorator')
         return decorator
     return wrapper
--- app/api/user_resources.py
+++ app/api/user_resources.py
@@ -4,7 +4,7 @@ from app import db
 from app.api.responses import response_with
 from app.api import responses as resp
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from app.api.decorators import admin_required
+from app.api.decorators import admin_required, resource_owner_required
 from app.exceptions import NotResourceOwnerError
 
 users=Blueprint('users',__name__)
@@ -24,7 +24,7 @@ def create():
     return response_with(resp.SUCCESS_201,{'user':user.to_json()})
 
 @users.route('/<int:id>')
-@jwt_required()
+@resource_owner_required()
 def find(id):
     logged_user=_check_and_find_user(id)
     found=db.get_or_404(User,id)
@@ -61,7 +61,7 @@ def login():
         return response_with(resp.UNAUTHORIZED_401, error='Login failed. username o password errati')
     
     if User.check_hash(json.get('password'),user.password):
-        additional_claims={'role':user.role.name}
+        additional_claims={'role':user.role.name, 'sub_id':user.id}
         token=create_access_token(identity=user.username, additional_claims=additional_claims)
         return response_with(resp.SUCCESS_201,value={'access_token':token})
 
