284461446c5daf06347f983d95587b07b357ac4f
Alfonso <alfonso.domenici@gmail.com>
2023-11-10 17:31:05 +0100

    default roles

--- pyposts.py
+++ pyposts.py
@@ -2,8 +2,10 @@ import os
 from app import create_app, db
 from dotenv import load_dotenv
 from flask_migrate import Migrate, upgrade
+from app.models.role import Role
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 
@@ -15,6 +17,11 @@ migrate = Migrate(app,db)
 def deploy():
     """Run deployment tasks."""
     upgrade()
+    default_rules = ['ADMIN','USER']
+    for r in default_rules:
+        if Role.query.filter_by(name=r).first() is None:
+            db.session.add(Role(name=r))
+    db.session.commit()
 
 @app.cli.command()
 def test():
